96a2d35002a39824ea406a20d2c2a9c1dbc40253
Paulo dos Santos <paulo@fazendadosoftware.com>
2019-01-17 15:44:28 -0200

    current state

---
+++ src/components/FactsheetCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Hi from factsheet card!</div>
+</template>
+
+<script>
+import LineChart from '@/components/LineChart'
+export default {
+  name: 'FactsheetCard',
+  components: { LineChart }
+}
+</script>
